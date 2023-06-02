1f4cca2a24d17ad3247d658dfdc25a6a2d698057
Adam Sampson <adam_sampson@b-f.com>
2023-06-02 13:11:44 -0400

    Update benchmark

--- test_conn.R
+++ test_conn.R
@@ -2,11 +2,13 @@ library(tidyverse)
 library(DBI)
 library(dbplyr)
 
-con <- dbConnect(odbc::odbc(), "CLPImpala", timeout = 10)
-date_tbl <- tbl(con, in_schema('aa_data_science_staging','dev_arm_desc_dates'))
-geo_tbl <- tbl(con, in_schema('aa_data_science_staging','dev_arm_desc_geo'))
-prod_tbl <- tbl(con, in_schema('aa_data_science_staging','dev_arm_desc_product'))
-fact_long_tbl <- tbl(con, in_schema('aa_data_science_staging','dev_arm_desc_values_long'))
+microbenchmark::microbenchmark({
+  con <- dbConnect(odbc::odbc(), "CLPImpala", timeout = 10)
+  date_tbl <- tbl(con, in_schema('aa_data_science_staging','dev_arm_desc_dates'))
+  geo_tbl <- tbl(con, in_schema('aa_data_science_staging','dev_arm_desc_geo'))
+  prod_tbl <- tbl(con, in_schema('aa_data_science_staging','dev_arm_desc_product'))
+  fact_long_tbl <- tbl(con, in_schema('aa_data_science_staging','dev_arm_desc_values_long'))
+}, times = 1L)
 
 microbenchmark::microbenchmark(
   avail_countries <- geo_tbl %>% select(country) %>% distinct() %>% pull(),
